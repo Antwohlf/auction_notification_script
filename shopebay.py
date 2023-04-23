1575b7fc0e8406c5f29b40f28c6bd0d49bdd13f9
Nicholas Jordan Wohlfeil <nick@Nicholass-MacBook-Air.local>
2023-04-23 13:06:24 -0400

    merge ebay

--- shopebay.py
+++ shopebay.py
@@ -10,51 +10,42 @@ from dotenv import load_dotenv
 def configure():
     load_dotenv()
 
-def search_shopebay():
+def search_shopebay(search_queries):
     configure()
     try:
         api = Connection(domain='svcs.ebay.com',appid=os.getenv('ebayauth'), config_file=None)
         with open('query.json') as file:
             payload = json.load(file)
         
-        response = api.execute('findItemsAdvanced', payload)
-
-        assert(response.reply.ack == 'Success')
-        assert(type(response.reply.timestamp) == datetime.datetime)
-        assert(type(response.reply.searchResult.item) == list)
         result = ""
-        title = ""
-        buyitnow = ""
-        condition = ""
-        watchers = ""
-        for item in response.reply.searchResult.item:
-            title = f'Title: {item.title}, Price: {item.sellingStatus.currentPrice.value}\n'
-            buyitnow = f'Buy it now available: : {item.listingInfo.buyItNowAvailable}\n'
-            try:
-                condition = f'Condition: {item.condition.conditionDisplayName}\n'
-            except:
-                condition = ""
-                pass
-            try:
-                watchers = f'Watchers: {item.listingInfo.watchCount}\n\n'
-            except:
-                watchers = ""
-                pass
-            # print(f'Title: {item.title}, Price: {item.sellingStatus.currentPrice.value}')
-            # print(f'Buy it now available: : {item.listingInfo.buyItNowAvailable}\n')
-            # try:
-            #     print(f'Condition: {item.condition.conditionDisplayName}')
-            # except:
-            #     pass
-            # try:
-            #     print(f'Watchers: {item.listingInfo.watchCount}\n')
-            # except:
-            #     pass
-            result += title + buyitnow + condition + watchers
-        
-        return result
+        for search in search_queries:
+            payload['keywords'] = search
+            response = api.execute('findItemsAdvanced', payload)
 
-
+            assert(response.reply.ack == 'Success')
+            assert(type(response.reply.timestamp) == datetime.datetime)
+            assert(type(response.reply.searchResult.item) == list)
+            title = ""
+            buyitnow = ""
+            condition = ""
+            watchers = ""
+            for item in response.reply.searchResult.item:
+                title = f'Title: {item.title}, Price: {item.sellingStatus.currentPrice.value}\n'
+                buyitnow = f'Buy it now available: : {item.listingInfo.buyItNowAvailable}\n'
+                try:
+                    condition = f'Condition: {item.condition.conditionDisplayName}\n'
+                except:
+                    condition = ""
+                    pass
+                try:
+                    watchers = f'Watchers: {item.listingInfo.watchCount}\n\n'
+                except:
+                    watchers = ""
+                    pass
+                
+                result += title + buyitnow + condition + watchers
+            
+            return result
 
     except ConnectionError as e:
         print(e)

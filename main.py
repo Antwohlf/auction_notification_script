48ff8c5b607a470fa0c9aee089930ad4cbd9979a
antwohlf <antwohlf@umich.edu>
2023-05-31 16:32:39 -0400

    Add try catch block to main for yagmail to fail gracefully

--- main.py
+++ main.py
@@ -16,34 +16,39 @@ import os
 # TODO enable last minute notifications for auctions
 
 def send_email(email, item_info):
-    # using yagmail from https://github.com/kootenpv/yagmail
-    yag = yagmail.SMTP(os.getenv('email'), oauth2_file='oauth.json')
+    try:
+        # using yagmail from https://github.com/kootenpv/yagmail
+        yag = yagmail.SMTP(os.getenv('email'), oauth2_file='oauth.json')
 
-    YEAR        = datetime.date.today().year     # the current year
-    MONTH       = datetime.date.today().month    # the current month
-    DATE        = datetime.date.today().day      # the current day
-    HOUR        = datetime.datetime.now().strftime("%H")   # the current hour
-    subject_time = "New GoodWill/Ebay Listings " + str(MONTH) + '/' + str(DATE) + '/' + str(YEAR) + ' - ' + str(HOUR) + ":00 UTC"
+        YEAR        = datetime.date.today().year     # the current year
+        MONTH       = datetime.date.today().month    # the current month
+        DATE        = datetime.date.today().day      # the current day
+        HOUR        = datetime.datetime.now().strftime("%H")   # the current hour
+        subject_time = "New GoodWill/Ebay Listings " + str(MONTH) + '/' + str(DATE) + '/' + str(YEAR) + ' - ' + str(HOUR) + ":00 UTC"
 
-    # contents can be a list of elements including strings and yagmail.inline objects
-    contents = []
-    for line in item_info.split('\n'):
-        if line.startswith('IMGURL:'):
-            # The line is an image URL, so add it as an HTML img tag with specified width and height
-            img_url = line[7:]
-            contents.append(f'<img src="{img_url}" alt="Image" width="200" height="200">')
-        elif line.startswith('AUCTIONEND:'):
-            # The line is an auction end time, so add it as a Google Calendar event link
-            auction_end = line[12:]
-            event_name = 'Auction+End'
-            event_url = f'https://www.google.com/calendar/render?action=TEMPLATE&text={event_name}&dates={auction_end}/{auction_end}'
-            contents.append(f'<a href="{event_url}">Add to Google Calendar</a>')
-        else:
-            # The line is not an image URL, so add it as a string
-            contents.append(line)
+        # contents can be a list of elements including strings and yagmail.inline objects
+        contents = []
+        for line in item_info.split('\n'):
+            if line.startswith('IMGURL:'):
+                # The line is an image URL, so add it as an HTML img tag with specified width and height
+                img_url = line[7:]
+                contents.append(f'<img src="{img_url}" alt="Image" width="200" height="200">')
+            elif line.startswith('AUCTIONEND:'):
+                # The line is an auction end time, so add it as a Google Calendar event link
+                auction_end = line[12:]
+                event_name = 'Auction+End'
+                event_url = f'https://www.google.com/calendar/render?action=TEMPLATE&text={event_name}&dates={auction_end}/{auction_end}'
+                contents.append(f'<a href="{event_url}">Add to Google Calendar</a>')
+            else:
+                # The line is not an image URL, so add it as a string
+                contents.append(line)
 
-    if contents:
-        yag.send(email, subject = subject_time, contents = contents)
+        if contents:
+            yag.send(email, subject = subject_time, contents = contents)
+
+    except Exception as e:
+        print(f"Failed to send email. Error: {e}")
+
 
 if __name__ == '__main__':
     profile_name = input('Select a profile: ')

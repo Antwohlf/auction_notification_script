d345df3887d7e1d3272f0d86aee62fb1c974a69f
antwohlf <antwohlf@umich.edu>
2024-03-02 23:36:00 -0500

    Integrate facebook script into main, clean up some other parts

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.shopgoodwill import search_shopgoodwill
 from src.shopebay import search_shopebay
 from src.shopcraigslist import search_shopcraigslist
+from src.shopumdispo import search_shopuofmdispo
+from src.shopfacebook import search_marketplace
 from cleantext import clean
 import yagmail
 import datetime
@@ -17,54 +19,7 @@ import re
 # TODO link on auction end date to add to google calendar
 # TODO enable last minute notifications for auctions
 
-def send_email(email, item_info):
-
-    #print(item_info)
-    try:
-        # using yagmail from https://github.com/kootenpv/yagmail
-        yag = yagmail.SMTP(os.getenv('email'), oauth2_file='oauth.json')
-        YEAR        = datetime.date.today().year     # the current year
-        MONTH       = datetime.date.today().month    # the current month
-        DATE        = datetime.date.today().day      # the current day
-        HOUR        = datetime.datetime.now().strftime("%H")   # the current hour
-        subject_time = "New Goodwill/Ebay Listings" + " " + str(MONTH) + '/' + str(DATE) + '/' + str(YEAR) + ' - ' + str(HOUR) + ":00 UTC"
-
-        # contents can be a list of elements including strings and yagmail.inline objects
-        contents = []
-        for line in item_info.split('\n'):
-            if line.startswith('IMGURL:'):
-                # The line is an image URL, so add it as an HTML img tag with specified width and height
-                img_url = line[7:]
-                contents.append(f'<img src="{img_url}" alt="Image" width="200" height="200">')
-            elif line.startswith('LINK:'):
-                link = line[5:]
-                print("LINK")
-                print(link)
-                print('\n')
-                title = re.search(r'^TITLE:.*', link, re.MULTILINE)
-                print("TITLE")
-                print(title)
-                print('\n')
-                '''
-                contents.append(f'<a href="{link}">' + title[6:] + '</a>')  '''
-            elif line.startswith('AUCTIONEND:'):
-                # The line is an auction end time, so add it as a Google Calendar event link
-                auction_end = line[12:]
-                event_name = 'Auction+End'
-                event_url = f'https://www.google.com/calendar/render?action=TEMPLATE&text={event_name}&dates={auction_end}/{auction_end}'
-                contents.append(f'<a href="{event_url}">Add to Google Calendar</a>')                
-            else:
-                # The line is not an image URL, so add it as a string
-                contents.append(line)
-
-        if contents:
-            yag.send(email, subject = subject_time, contents = contents)
-
-    except Exception as e:
-        print(f"Failed to send email. Error: {e}")
-
-## TODO FIX this naming when the above function is deleted
-def send_email_standardized(destination_email, results_shopgoodwill, results_ebay, results_shopcraigslist):
+def send_email_standardized(destination_email, results_shopgoodwill, results_ebay, results_shopcraigslist, results_uofmdispo, results_facebook):
     # using yagmail from https://github.com/kootenpv/yagmail
     yag = yagmail.SMTP(os.getenv('email'), oauth2_file='oauth.json')
 
@@ -78,6 +33,7 @@ def send_email_standardized(destination_email, results_shopgoodwill, results_eba
     # contents can be a list of elements including strings and yagmail.inline objects
     contents = []
 
+    '''
     # SHOPGOODWILL RUN
     contents.append("<h3 style='text-transform: uppercase;'>ShopGoodWill Results:</h3>\n")
     try:
@@ -147,13 +103,59 @@ def send_email_standardized(destination_email, results_shopgoodwill, results_eba
         print(f"Issue encountered with ShopCraigslist script - Error: {e}")
 
 
+    # SHOPUMDISPO RUN
+    contents.append("<h3 style='text-transform: uppercase;'>ShopUMDispo Results:</h3>\n")
+    try:
+        for item in results_uofmdispo:
+            # The line is an image URL, so add it as an HTML img tag with specified width and height
+            img_url = item["IMGURL"]
+            contents.append(f'<img src="{img_url}" alt="Image" width="200" height="200">')
+
+            title = item["TITLE"]
+            link = item["LINK"]
+            contents.append(f'<a href="{link}">' + title + '</a>')
+
+            price = item["PRICE"]
+            contents.append("Price: " + str(price))
+
+            auction_end = item["AUCTIONEND"]
+            event_name = 'Auction+End'
+            event_url = f'https://www.google.com/calendar/render?action=TEMPLATE&text={event_name}&dates={auction_end}/{auction_end}'
+            contents.append(f'<a href="{event_url}">Add to Google Calendar</a>') 
+    except Exception as e:
+        print(f"Issue encountered with UM Dispo script - Error: {e}")
+    '''
+
+    # SHOPFACEBOOK RUN
+    contents.append("<h3 style='text-transform: uppercase;'>ShopFacebook Results:</h3>\n")
+    try:
+        for item in results_facebook:
+            # The line is an image URL, so add it as an HTML img tag with specified width and height
+            img_url = item["IMGURL"]
+            contents.append(f'<img src="{img_url}" alt="Image" width="200" height="200">')
+
+            title = item["TITLE"]
+            link = item["LINK"]
+            contents.append(f'<a href="{link}">' + title + '</a>')
+
+            price = item["PRICE"]
+            contents.append("Price: " + str(price))
+
+            auction_end = item["AUCTIONEND"]
+            event_name = 'Auction+End'
+            event_url = f'https://www.google.com/calendar/render?action=TEMPLATE&text={event_name}&dates={auction_end}/{auction_end}'
+            contents.append(f'<a href="{event_url}">Add to Google Calendar</a>') 
+    except Exception as e:
+        print(f"Issue encountered with ShopFacebook script - Error: {e}")
+    
+
     if contents:
         yag.send(destination_email, subject = subject_time, contents = contents)
 
 if __name__ == '__main__':
     profile_name = input('Select a profile: ')
     profile_path = "profiles/" + profile_name + ".json"
-    gw_dupes, eb_dupes, cl_dupes = set(), set(), set()
+    gw_dupes, eb_dupes, cl_dupes, um_dupes, fb_dupes = set(), set(), set(), set(), set()
 
     while(True):
         with open(profile_path) as f:
@@ -162,13 +164,14 @@ if __name__ == '__main__':
         search_queries = profile["searchQueries"]
         destination_email = profile["email"]
 
-
-
         # NEW RESULTS FORMAT
         results_shopgoodwill = search_shopgoodwill(search_queries, gw_dupes)
         results_ebay = search_shopebay(search_queries, eb_dupes)
         results_craigslist = search_shopcraigslist(search_queries, cl_dupes)
+        #results_uofmdispo = search_shopuofmdispo(search_queries, um_dupes)
+        results_uofmdispo = {}
+        results_facebook = search_marketplace(search_queries, fb_dupes)
         
-        send_email_standardized(destination_email, results_shopgoodwill, results_ebay, results_craigslist)
+        send_email_standardized(destination_email, results_shopgoodwill, results_ebay, results_craigslist, results_uofmdispo, results_facebook)
 
         time.sleep(21600)
--- src/shopfacebook.py
+++ src/shopfacebook.py
@@ -1,49 +1,59 @@
 from playwright.sync_api import sync_playwright
 import json
 
-def search_marketplace():
+# TODO investigate why price changes overwrite the item title
+
+def search_marketplace(search_queries, fb_dupes):
     with open('query_templates/query_facebook.json') as file:
         payload = json.load(file)
+        location = "annarbor"
+
         with sync_playwright() as p:
             browser = p.chromium.launch()
             page = browser.new_page()
-            page.goto(f'https://facebook.com/marketplace/{payload["location"]}/search?query={payload["query"]}')
-            page.wait_for_selector('[aria-label="Collection of Marketplace items"]')
-
-            locators = page.locator('[aria-label="Collection of Marketplace items"] a')
-            el_count = locators.count()
-
-            entries = []
-            hrefs = []
-
-            for index in range(el_count):
-                element = locators.nth(index)
-                inner_text = element.inner_text()
-                href = element.get_attribute('href')
-
-                entries.append(inner_text)
-                hrefs.append(href)
-
             parsed_data = []
-            for entry, href in zip(entries, hrefs):
-                # Split entry based on newline character to separate title, price, and location
-                parts = entry.split('\n')
-                # Extract title, price, and location
-                title = parts[1]
-                price = parts[0]
-                location = parts[-1]
-                # Append parsed data as a dictionary
-                parsed_data.append({
-                    'title': title,
-                    'price': price,
-                    'location': location,
-                    'link': 'facebook.com' + href
-                })
 
-            for item in parsed_data:
-                print(item)
+            for query in search_queries:
+                page.goto(f'https://facebook.com/marketplace/{location}/search?query={query[0]}')
+                page.wait_for_selector('[aria-label="Collection of Marketplace items"]')
+
+                locators = page.locator('[aria-label="Collection of Marketplace items"] a')
+                el_count = locators.count()
+
+                entries = []
+                hrefs = []
+
+                for index in range(el_count):
+                    element = locators.nth(index)
+                    inner_text = element.inner_text()
+                    href = element.get_attribute('href')
+
+                    entries.append(inner_text)
+                    hrefs.append(href)
+
+                for entry, href in zip(entries, hrefs):
+                    # Split entry based on newline character to separate title, price, and location
+                    parts = entry.split('\n')
+                    # Extract title, price, and location
+                    title = parts[1]
+                    price = parts[0]
+                    location = parts[-1]
+                    # Append parsed data as a dictionary
+                    parsed_data.append({
+                        'TITLE': title,
+                        'PRICE': price,
+                        'LOCATION': location,
+                        'LINK': 'facebook.com' + href,
+                        'IMGURL': "None", 
+                        'AUCTIONEND': 'None'
+                    })
+
+            print(parsed_data[0])
+            #for item in parsed_data:
+            #    print(item)
             
             browser.close()
+            return parsed_data
 
 if __name__ == "__main__":
     search_marketplace()
